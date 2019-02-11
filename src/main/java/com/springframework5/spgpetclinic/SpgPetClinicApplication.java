0fc13515f976bfc200d8ffa91bd178af882e85f7
Alexandre Six Belem <alexandrenilton@gmail.com>
2019-02-11 20:56:19 -0200

    Initial Project Add

---
+++ src/main/java/com/springframework5/spgpetclinic/SpgPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.springframework5.spgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpgPetClinicApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/springframework5/spgpetclinic/SpgPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springframework5.spgpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
