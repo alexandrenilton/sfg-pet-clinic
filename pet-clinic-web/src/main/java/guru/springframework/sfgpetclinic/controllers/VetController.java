0c2c7dfa1d337dbecec2ed4061b19db21d21b2a9
Alexandre Six Belem <alexandrenilton@gmail.com>
2019-02-16 22:18:31 -0200

    Refactored @RequestMapping in Vet and Owner Controllers. Closes #21

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -3,10 +3,11 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVest(){
         return "vets/index";
     }
