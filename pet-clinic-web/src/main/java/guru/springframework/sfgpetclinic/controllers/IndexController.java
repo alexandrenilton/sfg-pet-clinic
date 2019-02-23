b1000e5b19dd2af4b6ebf229a92aca52ee1c86cd
Alexandre Six Belem <alexandrenilton@gmail.com>
2019-02-23 00:22:38 -0300

    List all vets.  List all owners was in a strong Controller (IndexController) changed it to OwnerController. Closes #14

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,23 +1,16 @@
 package guru.springframework.sfgpetclinic.controllers;
 
-import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    private final OwnerService ownerService;
 
-    public IndexController(OwnerService ownerService) {
-        this.ownerService = ownerService;
-    }
 
     @RequestMapping({"", "/", "index", "index.html"})
-    public String listOwners(Model model) {
-        model.addAttribute("owners", ownerService.findAll());
-        return "owners/index";
+    public String listOwners() {
+        return "index";
     }
 
 }
